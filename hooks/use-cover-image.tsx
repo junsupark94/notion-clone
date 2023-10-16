8e0c1768c70f4fbd8f0f3f1338a654e7f29c4225
Junsu Park <junsupark94@gmail.com>
2023-10-15 22:46:06 -0700

    Commit for deployment

---
+++ hooks/use-cover-image.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+type CoverImageStore = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  onReplace: (url: string) => void;
+  url?: string;
+};
+
+export const useCoverImage = create<CoverImageStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true, url: undefined }),
+  onClose: () => set({ isOpen: false, url: undefined }),
+  onReplace: (url) => set({ isOpen: true, url }),
+}));
